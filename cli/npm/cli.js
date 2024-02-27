d1eace26f099c026b750f5c781b2fe0a950ff89e
ObserverOfTime <chronobserver@disroot.org>
2024-02-27 12:11:00 +0200

    refactor(npm): bundle artifacts in package

--- cli/npm/cli.js
+++ cli/npm/cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const spawn = require("child_process").spawn;
+const { join } = require('path');
+const { spawn } = require('child_process');
+
 const executable = process.platform === 'win32'
-  ? 'tree-sitter.exe'
-  : 'tree-sitter';
+  ? 'tree-sitter.exe' : 'tree-sitter';
+
 spawn(
-  path.join(__dirname, executable),
+  join(__dirname, executable),
   process.argv.slice(2),
-  {stdio: 'inherit'}
+  { stdio: 'inherit' }
 ).on('close', process.exit)
